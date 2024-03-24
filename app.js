799a2102bae0e63c9b1720ae804737fcbf34079e
MaximeV <maximeverdy@gmail.com>
2024-03-24 17:03:20 +0100

    node server without Express

---
+++ app.js
@@ -0,0 +1,34 @@
+// Import the 'http' module, a core Node.js API, providing the essentials for handling HTTP transactions.
+// Direct utilization of this module illustrates a lower-level approach compared to frameworks like Express, offering fine-grained control over the HTTP lifecycle.
+const http = require('http');
+
+// Instantiate an HTTP server. The provided callback is an event listener that reacts to 'request' events,
+// illustrating the event-driven nature of Node.js. This pattern is crucial for non-blocking I/O operations.
+http.createServer((req, res) => {
+  // The response starts with writing headers. Setting the status to 200 indicates success.
+  // The 'Content-Type' header is critical for client interpretation of the response.
+  res.writeHead(200, {'Content-Type': 'text/html'});
+
+  // Parsing the request URL.This routing mechanism is rudimentary but demonstrates the foundational logic for handling different request paths.
+  const url = req.url;
+  switch(url) {
+    case '/about':
+      // Dynamic content generation is simplified for demonstration. In practice, template engines or server-side rendering solutions are used here.
+      res.write('<h1>About Me Page</h1>');
+      break;
+    case '/contact':
+      // This contact page example is static, but real-world applications should sanitize and validate user data rigorously.
+      res.write('<h1>Contact Page</h1>');
+      break;
+    default:
+      // The default case acts as a catch-all, an essential concept for ensuring a robust user experience by handling unexpected routes.
+      res.write('<h1>Landing Page</h1>');
+  }
+
+  // Conclude the response. This operation finalizes the response cycle, sending all pending data to the client.
+  // In complex scenarios, `res.end` might be called conditionally or as part of asynchronous operations.
+  res.end(); 
+
+// Binding the server to a port is a non-blocking operation, showcasing Node.js's non-blocking I/O model.
+// The callback confirms startup, serving as a basic example of asynchronous callbacks that are central to Node.js's design philosophy.
+}).listen(3000, () => console.log("Server started at port 3000"));
